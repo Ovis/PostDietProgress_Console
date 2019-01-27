cc8502fa6db57e8717a63fac08b33c2fa41879b0
Ovis <me+github@hitsujin.jp>
2019-01-27 19:21:46 +0900

    定義ファイルからのデータ取得処理追加

--- PostDietProgress/Program.cs
+++ PostDietProgress/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
 
 namespace PostDietProgress
 {
-    class Program
+    public static class Program
     {
+        enum HealthTag
+        {
+            WEIGHT = 6021, /* 体重 (kg) */
+            BODYFATPERF = 6022, /* 体脂肪率(%) */
+            MUSCLEMASS = 6023, /* 筋肉量(kg) */
+            MUSCLESCORE = 6024, /* 筋肉スコア */
+            VISCERALFATLEVEL2 = 6025, /* 内臓脂肪レベル2(小数点有り、手入力含まず) */
+            VISCERALFATLEVEL = 6026, /* 内臓脂肪レベル(小数点無し、手入力含む) */
+            BASALMETABOLISM = 6027, /* 基礎代謝量(kcal) */
+            BODYAGE = 6028, /* 体内年齢(歳) */
+            BONEQUANTITY = 6029 /* 推定骨量(kg) */
+        }
+
+        private static HttpClientHandler handler = new HttpClientHandler()
+        {
+            UseCookies = true
+        };
+        private static HttpClient httpClient = new HttpClient();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* 定義ファイルからID,パスワード,ClientID、ClientTokenを取得 */
+            string basePath = Directory.GetCurrentDirectory();
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(basePath)
+           .AddJsonFile("App.config.json", optional: true)
+           .Build();
+
+            Settings.TanitaUserID = configuration["Setting:TanitaUserID"];
+            Settings.TanitaUserPass = configuration["Setting:TanitaUserPass"];
+            Settings.TanitaClientID = configuration["Setting:TanitaClientID"];
+            Settings.TanitaClientSecretToken = configuration["Setting:TanitaClientSecretToken"];
+            Settings.DiscordWebhookUrl = configuration["Setting:DiscordWebhookUrl"];
+            Settings.OriginalWeight = Double.Parse(configuration["Setting:OriginalWeight"]);
+            Settings.GoalWeight = Double.Parse(configuration["Setting:GoalWeight"]);
+
         }
     }
 }
