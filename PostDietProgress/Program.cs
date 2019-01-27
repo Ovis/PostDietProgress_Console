bc87154b26a998db71664bd18eda8cde099eefb5
Ovis <me+github@hitsujin.jp>
2019-01-27 18:54:42 +0900

    Project作成

---
+++ PostDietProgress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PostDietProgress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
